8d9171941557fa1dabdc9beacfec2e2e7052ee7d
Helge Hess <me@helgehess.eu>
2017-03-07 22:47:12 +0100

    Type flags to Int32 to deal with Linux

--- Sources/JSONParser.swift
+++ Sources/JSONParser.swift
@@ -591,7 +591,7 @@ public struct JSONParser {
     }
 
     private func detectingFloatingPointErrors<T>(start loc: Int, _ f: () throws -> T) throws -> T {
-        let flags = FE_UNDERFLOW | FE_OVERFLOW
+        let flags : Int32 = FE_UNDERFLOW | FE_OVERFLOW
         feclearexcept(flags)
         let value = try f()
         guard fetestexcept(flags) == 0 else {
